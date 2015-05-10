79b2b4d70dd57bee26d4c4b292dedcc2d346b603
xinonixni <xinonix01@gmail.com>
2015-05-11 00:25:37 +0200

    json file

--- output.js
+++ output.js
@@ -1,7 +1,7 @@
 var jf = require('jsonfile');
 var util = require('util');
  
-var file = 'fing-output/discovery-output';
+var file = './fing-output/discovery-output';
 jf.readFile(file, function(err, obj) {
   console.log(util.inspect(obj))
 });
